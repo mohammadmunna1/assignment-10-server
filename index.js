9376aace9f9ae02310b25c056482807e698066ab
mohammad munna <mohammadmunna4141@gmail.com>
2023-05-05 22:16:59 +0600

    chef detailes

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.get('/chef', (req, res) => {
     res.send(chef)
 })
 
-
 app.get('/chef/:id', (req, res) => {
     const id = req.params.id;
     const selectedChef = chef.find(n => n.id == id)
